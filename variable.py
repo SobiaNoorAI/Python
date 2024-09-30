e4283a46ba982823d4082b07e000b4a15b5afbd3
Sobia Noor <sobianoor087@gmail.com>
2024-09-30 23:16:12 +0500

    Variables

--- variable.py
+++ variable.py
@@ -1,3 +1,11 @@
-<h1>Variables</h1>
+#<h1>Variables</h1>
 name ="wood"
 print(name)
+print(3+2)
+print(3-2)
+print(3*2)
+print(3**2) #power
+print (round ((3**2),4)) #round
+print(3/2) #return Float value
+print(3//2) #return Integer Value
+print(3%2) #moduler
