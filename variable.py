2316bfdcbbae4f7ccb8bcc7dc579972fd8cd05a6
Sobia Noor <sobianoor087@gmail.com>
2024-10-01 23:19:24 +0500

    Update variable.py string quotes

--- variable.py
+++ variable.py
@@ -10,7 +10,7 @@ print(3/2) #return Float value
 print(3//2) #return Integer Value
 print(3%2) #moduler
 #String--> collection of character
-nam1 = "Steve"
-nam2 = "Jobs"
+nam1 = 'Steve' #single quotes
+nam2 = "Jobs" #double quotes
 print(nam1 + nam2) # printing two strings togethor
 print(nam1 + "9") # printing string with int
