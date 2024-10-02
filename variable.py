7a65f089d788df5cd094a712e59ac15bbbc4c8be
Sobia Noor <sobianoor087@gmail.com>
2024-10-02 22:04:55 +0500

    Update variable.py Removing leading and trailing whitespaces in str

--- variable.py
+++ variable.py
@@ -11,7 +11,7 @@ print(3//2) #return Integer Value
 print(3%2) #moduler
 #String--> collection of character
 nam1 = 'Steve' #single quotes
-nam2 = "Jobs" #double quotes
+nam2 = "  Jobs  " #double quotes
 print(nam1 + nam2) # printing two strings togethor
 print(nam1 + "9") # printing string with int
 print(len(nam2)) # length of string len()
@@ -19,3 +19,4 @@ print(nam2[0]) # 1st character of str
 print(nam2[-1]) # last character of str
 print(nam2.upper()) # convert str to uppercase  
 print(nam2.lower()) # convert str to lowercase  
+print(nam2.strip()) # Removing leading and trailing whitespaces
