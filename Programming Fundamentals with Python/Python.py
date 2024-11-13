64a1cc970e40d69a39d3c1153614b5fabf8d5bfb
Sobia Noor <sobianoor087@gmail.com>
2024-11-13 21:22:20 +0500

    Update Python.py print min number

--- Programming Fundamentals with Python/Python.py
+++ Programming Fundamentals with Python/Python.py
@@ -1,1 +1,12 @@
+numbers_list =[]
+for i in range(5):
+  user_input=int(input('Enter a Number:'))
+  numbers_list.append(user_input)
 
+min_number = numbers_list[0]
+for current_number in numbers_list:
+  if current_number < min_number:
+    min_number = current_number
+
+print('min_number')  
+  
