526c1aef74066f26c1fe405ec61dcdadc0069881
Sobia Noor <sobianoor087@gmail.com>
2024-11-27 06:46:03 +0000

    Exercise 1: interest rate

--- Programming Fundamentals with Python/Python.py
+++ Programming Fundamentals with Python/Python.py
@@ -8,5 +8,14 @@ for current_number in numbers_list:
   if current_number < min_number:
     min_number = current_number
 
-print('min_number')  
-  
+print(min_number)  
+'''
+Exercise 1: The Bank of Python gives you a loan with an interest rate of 4%. 
+Write a Python program to read the loan amount from the user and calculate and print the total interest the customer will pay in a year. 
+(interest = amount * (interest rate / 100)).
+''' 
+amount = int(input("Enter Amount of Loan: ")) 
+interest_rate=4
+interest= interest = amount * (interest_rate /100)
+print(f'The interest you will pay in a year for Â£{amount} with an interest rate of {interest_rate}% is Â£{interest}')
+
